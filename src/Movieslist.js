197c7a08abcb46e2f0c2624fe567b197536783a0
Aryaman Sharma <200314arya@gmail.com>
2024-02-10 23:48:27 +0530

    GUI

--- src/Movieslist.js
+++ src/Movieslist.js
@@ -1,20 +1,39 @@
-  //This file is the fetch the movie data from the api 
-  //this will make an block on our web page in which all the  movie card will be displayed.
-  //THis filw will be the parent of Moviecard Component.
-  //to transfer data from this file to movie card using props.
-  import React from 'react'
-  import MovieCards from './MovieCards'
-  
-  export default function Movieslist({movies}) {
-   
+import React, { useState } from 'react';
+import MovieCards from './MovieCards';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons';
 
-    return (
+export default function Movieslist({ movies }) {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  return (
+    <>
+      <div className='hamburger-menu' onClick={toggleMenu}>
+        <FontAwesomeIcon icon={menuOpen ? faTimes : faBars} />
+      </div>
+
+      {menuOpen ? (
+        <div className="menu">
+          <button className='buttons1'>Rating Ascending</button>
+          <button className='buttons'>Rating Descending</button>
+          <button className='buttons'>Votes Ascending</button>
+          <button className='buttons'>Votes Descending</button>
+          <button className='buttons'>Latest Release</button>
+          <button className='buttons'>Old</button>
+        </div>
+      ) : null}
+
       <div>
         <ul className='movies-list'>
-          {movies.map((movie)=>(
-            <MovieCards key={movie.id} movie={movie}/>
+          {movies.map((movie) => (
+            <MovieCards key={movie.id} movie={movie} />
           ))}
         </ul>
       </div>
-    );
-  }
+    </>
+  );
+}
